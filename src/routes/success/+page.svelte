389830be3db4a6c22301c2843501999cddc4f8e7
ucupe <yusufkur98@gmail.com>
2025-02-11 10:08:15 +0700

    update succes

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -11,14 +11,14 @@
             animation-iteration-count: 3;
         }
 </style>
-<div class="">
+
 
   <!-- <Grid>
     <Row>
       <h1>Thank you. Your registration is complete.</h1>
     </Row>
   </Grid> -->
-  <body class="flex items-center justify-center min-h-screen">
+  <div class="flex items-center justify-center min-h-full">
     <div class="w-96 p-8 bg-white shadow-xl ring-2 ring-gray-900/5 rounded-2xl text-center ">
         <svg class="mx-auto mb-4 text-green-500 w-16 h-16v animate-bounce " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
@@ -27,5 +27,4 @@
         <p class="mt-2 text-gray-600 ">Pendaftaran anda telah selesai</p>
         <a href="/" class="inline-block px-6 py-2 mt-6 text-white bg-blue-500 rounded-lg hover:bg-blue-600">Kembali ke Beranda</a>
     </div>
-</body>
 </div>
