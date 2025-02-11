6b86d7e9124e92f688ff5723946a25ad23dcba3e
ucupe <yusufkur98@gmail.com>
2025-02-11 10:14:58 +0700

    update page succes vercel notworking

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -18,7 +18,7 @@
       <h1>Thank you. Your registration is complete.</h1>
     </Row>
   </Grid> -->
-  <div class="flex items-center justify-center min-h-full">
+  <div class="flex items-center justify-center h-screen">
     <div class="w-96 p-8 bg-white shadow-xl ring-2 ring-gray-900/5 rounded-2xl text-center ">
         <svg class="mx-auto mb-4 text-green-500 w-16 h-16v animate-bounce " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
